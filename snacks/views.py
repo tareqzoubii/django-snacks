9a8a1bde08a85a7232bf9e75198b7fba0666dfea
tareqzoubii <tareqzoubi4@gmail.com>
2022-11-28 10:39:43 +0200

    V1.0

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView # import template view first
 
 # Create your views here.
+# Now i Need To Create a class
+class HomePage(TemplateView):
+     template_name = 'home.html'
+
+# create another class for about page
+class AboutPage(TemplateView):
+     template_name = 'about.html'
