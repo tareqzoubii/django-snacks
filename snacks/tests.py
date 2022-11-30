ea3fdd0fee58b0eb3307ad1e5605ad3bebdc38e6
tareqzoubii <tareqzoubi4@gmail.com>
2022-11-30 10:46:53 +0200

    V3.0

--- snacks/tests.py
+++ snacks/tests.py
@@ -4,19 +4,23 @@ from django.urls import reverse
 # Create your tests here.
 
 class snackTests(SimpleTestCase):
-     def homePageStatus(self):
+     def test_homePageStatus(self):
           url = reverse('home')
           responce = self.client.get(url)
           self.assertEqual(responce.status_code, 200)
-     def aboutPageStatus(self):
+     
+     def test_aboutPageStatus(self):
           url = reverse('about')
           responce = self.client.get(url)
           self.assertEqual(responce.status_code, 200)
-     def homePageTemplate(self):
+     
+     def test_homePageTemplate(self):
           url = reverse('home')
           responce = self.client.get(url)
-          self.assertTemplateUsed(responce, 'home')
-     def aboutPageTemplate(self):
-          url = reverse('home')
+          print(responce)
+          self.assertTemplateUsed(responce, 'home.html')
+     
+     def test_aboutPageTemplate(self):
+          url = reverse('about')
           responce = self.client.get(url)
-          self.assertTemplateUsed(responce, 'about')
+          self.assertTemplateUsed(responce, 'about.html')
